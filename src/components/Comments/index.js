3de0e55244aaf25e2c0a96ff8d72440d2d784699
sudhir-kumar-kalivarapu <sudhirkalivarapu@gmail.com>
2023-04-16 16:29:14 +0000

    comments-app

---
+++ src/components/Comments/index.js
@@ -0,0 +1,137 @@
+import {Component} from 'react'
+import {v4} from 'uuid'
+
+import CommentItem from '../CommentItem'
+
+import './index.css'
+
+const backgroundColors = [
+  'amber',
+  'blue',
+  'orange',
+  'emerald',
+  'teal',
+  'red',
+  'light-blue',
+]
+
+class Comments extends Component {
+  state = {
+    nameInput: '',
+    commentInput: '',
+    commentsList: [],
+  }
+
+  deleteComment = commentId => {
+    const {commentsList} = this.state
+
+    this.setState({
+      commentsList: commentsList.filter(comment => comment.id !== commentId),
+    })
+  }
+
+  toggleIsLiked = id => {
+    this.setState(prevState => ({
+      commentsList: prevState.commentsList.map(eachComment => {
+        if (eachComment.id === id) {
+          return {...eachComment, isLiked: !eachComment.isLiked}
+        }
+        return eachComment
+      }),
+    }))
+  }
+
+  renderCommentsList = () => {
+    const {commentsList} = this.state
+
+    return commentsList.map(eachComment => (
+      <CommentItem
+        key={eachComment.id}
+        commentDetails={eachComment}
+        toggleIsLiked={this.toggleIsLiked}
+        deleteComment={this.deleteComment}
+      />
+    ))
+  }
+
+  onAddComment = e => {
+    e.preventDefault()
+    const {nameInput, commentInput} = this.state
+    const randomBgColor = `initial-container ${
+      backgroundColors[Math.ceil(Math.random() * backgroundColors.length - 1)]
+    }`
+    const newComment = {
+      id: v4(),
+      name: nameInput,
+      comment: commentInput,
+      date: new Date(),
+      isLiked: false,
+      initialClassName: randomBgColor,
+    }
+
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newComment],
+      nameInput: '',
+      commentInput: '',
+    }))
+  }
+
+  onChangeCommentInput = e => {
+    this.setState({
+      commentInput: e.target.value,
+    })
+  }
+
+  onChangeNameInput = e => {
+    this.setState({
+      nameInput: e.target.value,
+    })
+  }
+
+  render() {
+    const {nameInput, commentInput, commentsList} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="components-container">
+          <h1 className="app-heading">Comments</h1>
+          <div className="comment-inputs">
+            <form className="form" onSubmit={this.onAddComment}>
+              <p className="form-description">
+                Say something about 4.0 Technologies
+              </p>
+              <input
+                type="text"
+                className="name-input"
+                placeholder="Your Name"
+                value={nameInput}
+                onChange={this.onChangeNameInput}
+              />
+              <textarea
+                placeholder="Your Comment"
+                className="comment-input"
+                value={commentInput}
+                onChange={this.onChangeCommentInput}
+              />
+              <button type="submit" className="add-button">
+                Add Comment
+              </button>
+            </form>
+            <img
+              className="image"
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+              alt="comments"
+            />
+          </div>
+          <hr className="line" />
+          <p className="heading">
+            <span>{commentsList.length}</span>
+          </p>
+          <ul className="comments-list">{this.renderCommentsList()}</ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Comments
